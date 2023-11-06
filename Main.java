bdc25afd58e3bc1fbf83e27ac56a6788129c8a37
web-browser1 <1374904+web-browser1@users.noreply.github.com>
2023-11-06 17:48:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,14 @@
 import java.util.*;
 
-//import javax.swing.JFrame;
-
-//import javax.swing.*;
-
-// import java.awt.Dimension;
 
 
 public class Main
-//implements ActionListener
+
 {
 	
 	
 	final int FRAME_WIDTH = 300;
 	final int FRAME_HEIGHT = 300;
-	
-	// private JLabel nameLabel = new JLabel("Namn: ");
-	
-	
-	/*public void actionPerformed(ActionEvent e) 
-	{
-		
-		System.out.println("Click!");
-		
-		if(e.getSource() == button) 
-		{
-			
-			System.out.println("Click btn!");
-		}
-		
-		
-	}*/
 	
 	
 	final int TEXT_WIDTH = 20;
@@ -38,65 +16,9 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
 		
-		/*
-		JFrame frame = new JFrame();
-		
-		
-		
-		
-		// JTextField textField = new JTextField(TEXT_WIDTH);
+		BankLogic bank = new BankLogic();
 
-		// String text = textField.getText();
-		
-		// tlabel.setText("text " + text);
-		
-		
-		
-		
-		
-		JButton button = new JButton("btn");
-		
-		///  button.addActionListener(this);
-		
-		JLabel label = new JLabel("text");
-		
-		JPanel panel = new JPanel();
-		
-		// 3
-		// panel.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
-		
-		
-		panel.add(button);
-		panel.add(label);
-		
-		
-		frame.add(panel);
-		
-		
-		// 3
-		// frame.pack();
-		
-		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		
-		
-		
-		frame.setLocationRelativeTo(null); // center screen
-		
-		
-		
-		frame.setTitle("windows title");
-		
-		frame.setDefaultCloseOperation
-		(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setVisible(true);
-		
-		*/
-		
-		
-		
 		
 	}
 	
